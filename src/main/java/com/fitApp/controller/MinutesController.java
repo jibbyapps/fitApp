db174b6ec7640a8984e72fc46533e080d3a02d27
vitaliiromanchenko <packman2234@gmail.com>
2016-04-06 06:06:38 +0300

    my fit app

--- src/main/java/com/fitApp/controller/MinutesController.java
+++ src/main/java/com/fitApp/controller/MinutesController.java
@@ -1,9 +1,15 @@
 package com.fitApp.controller;
 
+import model.Activity;
 import model.Exercise;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by vitaliiromanchenko on 26.03.16.
@@ -19,6 +25,26 @@ public class MinutesController {
         return "addMinutes";
     //    return "forward: addMoreMinutes.html";
       //  return "redirect: addMoreMinutes.html";
+    }
+
+
+    @RequestMapping(value = "/activities",method = RequestMethod.GET)
+    public @ResponseBody List<Activity> findAllActivities(){
+        List<Activity> activities = new ArrayList<Activity>();
+        Activity run = new Activity();
+        run.setDesc("Run");
+        activities.add(run);
+
+        Activity bike = new Activity();
+        bike.setDesc("Bike");
+        activities.add(bike);
+
+        Activity swim = new Activity();
+        swim.setDesc("Swim");
+        activities.add(swim);
+
+        return activities;
+
     }
 
 //    @RequestMapping(value = "/addMoreMinutes")
---
+++ src/main/java/model/Activity.java
@@ -0,0 +1,21 @@
+package model;
+
+/**
+ * Created by vitaliiromanchenko on 05.04.16.
+ */
+public class Activity {
+
+    private String desc;
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+
+
+
+}
